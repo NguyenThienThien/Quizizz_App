bf2a2b209c757b68042496d32e912775a17774b1
Nguyen Thien Thien <kinaad24@gmail.com>
2024-09-25 11:14:15 +0700

    #3 code base screen navigation

---
+++ app/src/main/java/com/example/quizizz_app/navigation/ScreenName.kt
@@ -0,0 +1,9 @@
+package com.example.quizizz_app.navigation
+
+enum class ScreenName(val route: String) {
+    WelcomeScreen("WelcomeScreen"),
+    LoginScreen("LoginScreen"),
+    SignUpScreen("SignUpScreen"),
+    HomeScreen("HomeScreen"),
+    QuizScreen("QuizScreen"),
+}
---
+++ app/src/main/java/com/example/quizizz_app/navigation/ScreenNavigation.kt
@@ -0,0 +1,28 @@
+package com.example.quizizz_app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quizizz_app.activities.LoginScreen
+import com.example.quizizz_app.activities.QuizScreen
+import com.example.quizizz_app.activities.SignUpScreen
+import com.example.quizizz_app.activities.WelcomeScreen
+
+@Composable
+fun ScreenNavigation(){
+    val navController = rememberNavController()
+    val context = LocalContext.current
+
+    NavHost(
+        navController = navController,
+        startDestination = "WelcomeScreen"
+    ){
+        composable(ScreenName.WelcomeScreen.route){ WelcomeScreen(navController) }
+        composable(ScreenName.LoginScreen.route){ LoginScreen(navController) }
+        composable(ScreenName.SignUpScreen.route){ SignUpScreen(navController) }
+        composable(ScreenName.HomeScreen.route){ QuizScreen() }
+        composable(ScreenName.QuizScreen.route){ QuizScreen() }
+    }
+}
